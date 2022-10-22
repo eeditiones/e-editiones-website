fe37160c0c0a805d69238a3febbd0a16ed752658
Wolfgang Meier <wolfgang@existsolutions.com>
2022-10-22 22:59:37 +0200

    Add projects map

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,7 @@ module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("scripts");
 
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
