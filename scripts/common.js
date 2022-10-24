983b7d6f44d1cfe6e03ca58bbe451a4d491cd2aa
wolfgangmm <wolfgangmm@users.noreply.github.com>
2022-10-24 13:06:57 +0000

    deploy: 5762e80aa756abef9910ad4128cd7747081bbb6a

---
+++ scripts/common.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // enable tooltips everywhere
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+});
