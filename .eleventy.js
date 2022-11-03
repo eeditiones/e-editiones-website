a47a136e8c0852144caff2b4f22206893906b5b3
Wolfgang Meier <wolfgang@exist-db.org>
2022-11-03 09:39:53 +0100

    Fix broken images

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,7 @@ const pluginTP = require('@teipublisher/pb-eleventy-plugin');
 
 module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
+  eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("resources");
 
